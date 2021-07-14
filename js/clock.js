95ac23e7e91cd51e52fff6fb2d4661476db6b723
Jeraden <61553447+Jeraden@users.noreply.github.com>
2021-07-14 12:37:39 +0900

    Add Clock feature
    Add Clock feature to login greeting page.

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("h2#clock");
+
+
+function getClock() {
+	const dateObject = new Date();
+	const hours = String(dateObject.getHours()).padStart(2, "0");
+	const minutes = String(dateObject.getMinutes()).padStart(2, "0");
+	const seconds = String(dateObject.getSeconds()).padStart(2, "0");
+	clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000 ,1000, 1000);
+
